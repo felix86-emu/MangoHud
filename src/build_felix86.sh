b9f7a25b5bb0c9e336a3b4b59425ae098d7c3fe3
Paris Oplopoios <21157395+OFFTKP@users.noreply.github.com>
2026-08-08 22:30:21 +0300

    Add build commands

---
+++ src/build_felix86.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ meson setup build \
+    -Dprefix=/opt/felix86 \
+    -Dlibdir=lib/riscv64-linux-gnu \
+    -Dappend_libdir_mangohud=true \
+    -Dwith_fex=true \
+    -Dfex_label=felix86
+
+ninja -C build -j$(nproc)
+
+meson install -C build --destdir /tmp/staging
+
+echo ""
+echo "Files installed in /tmp/staging"
